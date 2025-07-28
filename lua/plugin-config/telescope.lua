efac51a4d864f611b24b223014aae9c9647d72fa
pyxyzc <pyxyzc@gmail.com>
2025-07-29 01:34:51 +0800

    use default normal mode in Ctrl F/P

--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -7,7 +7,7 @@ end
 telescope.setup({
 	defaults = {
 		-- 打开弹窗后进入的初始模式，默认为 insert，也可以是 normal
-		initial_mode = "insert",
+		initial_mode = "normal",
 		-- 窗口内快捷键
 		mappings = require("keybindings").telescopeList,
 	},
