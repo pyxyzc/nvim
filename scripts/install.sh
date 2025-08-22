0967f6733a075d34078355a831c677de37171a4d
pengyao <pyxyzc@gmail.com>
2025-08-23 00:19:18 +0800

    Add unsupported OS

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 # How to use?
 # bash install.sh [...] [-m] [...]
 
+# 使用方法
 for arg in "$@"; do
   [[ "$arg" == "-h" || "$arg" == "--help" ]] && {
     echo "Usage: "
@@ -12,12 +13,18 @@ for arg in "$@"; do
   }
 done
 
+# =============================================
+# 前置
+# =============================================
+
+# 判断是否是root用户，是否需要使用sudo
 if [[ $EUID -eq 0 ]]; then
     SUDO=""
 else
     SUDO="sudo"
 fi
 
+# 指令集架构
 ARCH="$(uname -m)"
 
 if [[ -f /etc/os-release ]]; then
@@ -28,13 +35,21 @@ else
     exit 1
 fi
 
+# 操作系统
 case $OS in
-    centos|rhel|rocky|almalinux)
+    centos)
         ;;
-    ubuntu|debian)
+    ubuntu)
+        ;;
+    openEuler)
         ;;
     *) echo "Unsupported Operating System: $OS" >&2; exit 1 ;;
 esac
+
+
+# ==================================================
+# 下载nvim
+# ==================================================
 
 URL=""
 DIR=""
@@ -55,7 +70,7 @@ echo "Downloading Neovim nightly for $ARCH ..."
 curl -LO "$URL"
 
 echo "Extracting ..."
-tar -zxvf "$DIR.tar.gz"                                                                                                                                                                                   
+tar -zxvf "$DIR.tar.gz"                                                                                                                                                 
 echo "Installing to /opt/nvim ..."
 $SUDO mkdir -p /opt/nvim
 $SUDO rsync -a "$DIR"/ /opt/nvim/
@@ -64,8 +79,10 @@ $SUDO ln -sf /opt/nvim/bin/nvim /usr/local/bin/nvim
 echo "Done!"
 nvim --version
 
-echo "=================================================="
-
+# ==================================================
+# 安装nvim
+# ==================================================
+#
 mkdir -p ~/.config
 [[ -d ~/.config/nvim ]] && rm -rf ~/.config/nvim
 [[ -d ~/.local/share/nvim ]] && rm -rf ~/.local/share/nvim
@@ -74,12 +91,11 @@ cp -r "$(dirname "$(dirname "$(realpath "$0")")")" ~/.config/nvim
 
 echo "nvim config copied to ~/.config/nvim"
 
-echo "=================================================="
 
 if ! command -v nvm >/dev/null 2>&1; then
   echo ">>> npm not found ..."
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash -s -- --no-use
-  
+
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 fi
@@ -103,16 +119,18 @@ npm  --version
 echo "=================================================="
 
 case $OS in
-    centos|rhel|rocky|almalinux)
+    centos)
         if command -v dnf >/dev/null 2>&1; then
             $SUDO dnf install -y python3-pip
         else
             $SUDO yum install -y python3-pip
         fi
         ;;
-    ubuntu|debian)
+    ubuntu)
         $SUDO apt-get update
         $SUDO apt-get install -y python3-pip
+        ;;
+    openEuler)
         ;;
     *) echo "Unsupported Operating System: $OS" >&2; exit 1 ;;
 esac
@@ -124,19 +142,24 @@ else
     exit 1
 fi
 
-echo "=================================================="
-
+# ==================================================
+# 安装fd和ripgrep
+# ==================================================
 case $OS in
-    centos|rhel|rocky|almalinux)
+    centos)
         if command -v dnf >/dev/null 2>&1; then
             $SUDO dnf install -y ripgrep fd-find
         else
             $SUDO yum install -y ripgrep fd-find
         fi
         ;;
-    ubuntu|debian)
+    ubuntu)
         $SUDO apt-get update
         $SUDO apt-get install -y ripgrep fd-find
+        ;;
+    openEuler)
+        $SUDO pip install ripgrep
+        $SUDO pip install fd
         ;;
     *) echo "Unsupported Operating System: $OS" >&2; exit 1 ;;
 esac
